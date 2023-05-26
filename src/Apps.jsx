ebeee9e8b11bf289bc1db365e9894223d798b122
Ehtisham Ali <40139972+Ehtish@users.noreply.github.com>
2023-05-26 11:31:08 +0500

    Add files via upload

---
+++ src/Apps.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import NotFound from "./components/NotFound";
+import Addactivity from "./components/Addactivity";
+import Exercise from "./components/Exercise";
+import Logout from "./components/Logout";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import Signup from "./components/Signup"; //usman
+import Login from "./components/Login"; //usman
+import App from "./components/App";
+
+function Apps() {
+  // const [first, setfirst] = useState("");
+  // const location = useLocation();
+  // setfirst(location.pathname);
+  // const rout = location.pathname;
+  // console.log(rout);
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<App />} />
+          <Route path="/addactivity" element={<Addactivity />} />
+          <Route path="/exercise" element={<Exercise />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+
+export default Apps;
